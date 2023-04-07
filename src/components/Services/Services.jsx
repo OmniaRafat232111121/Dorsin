1a0a64cec1040b191c0c76d0bd3aba5b0af92f3c
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2023-04-07 05:32:32 +0200

    services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Data } from '../../data'
+import '../Services/Services.css'
+import ServiceCard from '../ServiceCard';
+import {AiOutlineArrowRight} from 'react-icons/ai';
+import online from '../../assets/online.svg'
 const Services = () => {
     //mappping items on Data
     const item=Data.serv.map((itembox)=>{
 
         return(
             <div className='col-md-4'>
-                {itembox.title}
-                {itembox.text}
+                <ServiceCard title={itembox.title} text={itembox.text} icon={itembox.icon}/>
 
             </div>
         )
@@ -25,6 +28,29 @@ const Services = () => {
         </div>
         <div className='row'>
             {item}
+
+        </div>
+        <div className='row last'>
+            <div className='col-md-6'>
+                <h3>A digital web design studio creating 
+                    modern & engaging online experiences</h3>
+                <p>Separated they live in Bookmarksgrove right at the coast of the
+                     Semantics, a large language ocean. A small river named Duden
+                      flows by their place and supplies it with the necessary regelialia.</p>
+
+            <ul>
+                <li>We put a lot of effort in design.</li>
+                <li>The most important ingredient of successful website.</li>
+                <li>Sed ut perspiciatis unde omnis iste natus error sit.</li>
+               <li>Submit Your Orgnization.</li>
+            </ul> 
+            <button>
+                Learn More <AiOutlineArrowRight/>
+            </button>
+            </div>
+            <div className='col-md-6'>
+             <img src={online} alt="online"/>
+            </div>
 
         </div>
 
--- src/data.js
+++ src/data.js
@@ -1,27 +1,35 @@
+import { faDiamond,faFile,faSackDollar,faPlaneArrival,faTableList,faPaintRoller } from '@fortawesome/free-solid-svg-icons'
+
 export const Data={
     serv:[
         {
+            icon:faDiamond,
            title:'Digital Design',
            text:`Some quick example text to build on the card title and make up the bulk of the card's content. Moltin gives you the platform.`,
 
         },
         {
+            icon:faFile,
             title:'unlimitedColor',
             text:'Credibly brand standards compliant users without extensible services. Anibh euismod tincidunt ut laoreet.',
         },
         {
+            icon:faSackDollar,
             title:'Stardegysolution',
             text:'Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean necessary regelialia.',
         },{
+            icon:faPlaneArrival,
             title:'Awesome Support',
             text:'It is a paradisematic country, in which roasted parts of sentences fly into your mouth leave for the far World.',
 
         },
         {
+            icon:faTableList,
             title:'Truly Multipurpose',
             text:'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.'
         },
         {
+            icon:faPaintRoller,
             title:"Easy to Customize",
             text:'Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia.'
         }
